69cb162a7860ba7615ab6a0752031427835c0d31
Shanidh <shanidhkhanmepathur@gmail.com>
2024-07-02 15:13:55 +0530

    JSON added

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ server.get("/", (req, res) => {
 	console.log(req.url);
 	res.status(200).send("Hello World");
 });
+server.get("/employee", (req, res) => {
+	console.log(req.url);
+	res.status(200).send("My Name is Shanidh Khan");
+});
+server.get("/getData", (req, res) => {
+	let data = {
+		profile: {
+			name: "Shanidh",
+			age: 23,
+		},
+	};
+	console.log(data.profile.name);
+	res.status(200).send(data);
+});
 server.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
