84a46ac08650577b03c9783cd184c2f081eb6763
Shanidh <shanidhkhanmepathur@gmail.com>
2024-07-03 11:22:03 +0530

    server file1

--- app.ts
+++ app.ts
@@ -11,24 +11,3 @@ server.get("/employee", (req: Request, res: Response) => {
 	console.log(req.url);
 	res.status(200).send("My Name is Shanidh Khan");
 });
-
-interface Profile {
-	name: string;
-	age: number;
-}
-interface Data {
-	profile: Profile;
-}
-server.get("/getData", (req: Request, res: Response) => {
-	let data: Data = {
-		profile: {
-			name: "Shanidh",
-			age: 23,
-		},
-	};
-	console.log(data.profile.name);
-	res.status(200).send(data);
-});
-server.listen(3000, () => {
-	console.log("Server is running on port 3000");
-});
