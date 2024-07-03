6cc4b89aefcb8e8bba5ed278669101fce025e7ec
Shanidh <shanidhkhanmepathur@gmail.com>
2024-07-03 11:21:02 +0530

    typescript final

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 const express = require("express");
 import { Request, Response } from "express";
+
 const server = new express();
+
 server.get("/", (req: Request, res: Response) => {
 	console.log(req.url);
 	res.status(200).send("Hello World");
